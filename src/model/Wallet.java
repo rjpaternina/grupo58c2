70a0ca73aacee76862ec9847f201f81a212ddcd1
rjpaternina <rjpaternina@gmail.com>
2021-07-11 20:29:02 -0500

    First update

---
+++ src/model/Wallet.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Wallet {
+    
+    public static final int CAPACIDAD_MAXIMA = 1000000;
+
+    private int saldo;
+    private boolean tieneLimite;
+
+    public Wallet() {
+        super();
+        saldo = 0;
+        tieneLimite = true;
+    }
+
+    public int getSaldo(){
+        return saldo;
+    }    
+
+    public boolean getTieneLimite (){
+        return tieneLimite;
+    }
+    public void setTieneLimite(boolean  newTieneLimite) {
+        this.tieneLimite = newTieneLimite;
+    }
+    public String saveMoney(int value) {
+        if (saldo + value > CAPACIDAD_MAXIMA && tieneLimite) {
+            return "No se puede superar el limite " + CAPACIDAD_MAXIMA;
+        }    
+        saldo += value; // saldo = saldo + value
+        return "Transaccion exitosa, nuevo saldo " + saldo;
+    }     
+
+    public String takeMoney (int value) {
+        if (saldo >= value) {
+            saldo -= value;
+            return  "Transaccion exitosa, nuevo saldo " + saldo;
+        }
+        return "Saldo insuficiente"; 
+    }
+
+    public String breakLimits() {   
+        if(!tieneLimite) {
+            return "Tu cuenta no tiene limites!";
+        }    
+        if (saldo >= 10000) {
+            saldo -= 10000; 
+            setTieneLimite(false); //Tiene Limites = False
+            return "Has roto los limites";
+        }
+        return "No tienes saldo suficiente :( ";
+    }
+
+}
+       
